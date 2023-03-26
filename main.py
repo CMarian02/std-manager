dacab04e4692577e240cc37a8487c26be1e7d6ff
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2023-03-26 15:21:13 +0300

    some labels, one buttons, some text and some style for this, on Login Page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtWidgets, QtCore
+from PyQt6 import QtWidgets, QtCore, QtGui
 import screeninfo
 import sys
 
@@ -15,27 +15,44 @@ class MyApp(QtWidgets.QMainWindow):
         self.setWindowTitle("STD-Manager")
         self.resize(900, 650)
         self.setMinimumSize(800, 650)
+        self.setMaximumSize(800, 650)
 
         #-----------SING IN-----------
 
-        # title
+        # labels
         self.log_title = QtWidgets.QLabel('SING IN', self)
-        self.log_title.setGeometry(385, 70, 130, 50)
+        self.log_title.setGeometry(305, 70, 190, 90)
         self.log_title.setObjectName('log_title')
         self.inp_text_name = QtWidgets.QLabel('USERNAME', self)
-        self.inp_text_name.setGeometry(280, 165, 100, 50)
+        self.inp_text_name.setGeometry(150, 255, 130, 50)
         self.inp_text_name.setObjectName('input_text')
         self.inp_text_pass = QtWidgets.QLabel('PASSWORD', self)
-        self.inp_text_pass.setGeometry(280, 195, 100, 50)
+        self.inp_text_pass.setGeometry(150, 315, 130, 50)
         self.inp_text_pass.setObjectName('input_text')
+        self.vers_text = QtWidgets.QLabel('version 0.1', self)
+        self.vers_text.setGeometry(735, 630, 70, 20)
+        self.vers_text.setObjectName('version_text')
+        self.info_title = QtWidgets.QLabel('INFO', self)
+        self.info_title.setGeometry(680, 150, 150, 70)
+        self.info_title.setObjectName('info_title')
+        self.info_text = QtWidgets.QLabel(self)
+        self.info_text.setGeometry(600, 200, 190, 300)
+        self.info_text.setObjectName('info_text')
         # intputs
         self.inp_name = QtWidgets.QLineEdit(self)
-        self.inp_name.setGeometry(390, 180, 130, 20)
+        self.inp_name.setGeometry(280, 260, 200, 40)
         self.inp_name.setObjectName('input_logpg')
         self.inp_pass = QtWidgets.QLineEdit(self)
-        self.inp_pass.setGeometry(390, 210, 130, 20)
+        self.inp_pass.setGeometry(280, 320, 200, 40)
         self.inp_pass.setObjectName('input_logpg')
-        self.inp_pass.password
+        self.inp_pass.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+
+        #buttons
+        self.btn_log = QtWidgets.QPushButton(self)
+        self.btn_log.setText('LOGIN')
+        self.btn_log.setGeometry(280, 390, 200, 50)
+        self.btn_log.setObjectName('btn_log')
+
 
 
 
