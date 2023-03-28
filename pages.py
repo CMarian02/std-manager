b1b67bc42565c522880869e9bf07b0ca04911fa5
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2023-03-28 18:13:15 +0300

    create a base page, where you can reset your password, create a functions to check if your password is strong, if it is, you can reset and change in your db file, design is just a shell

--- pages.py
+++ pages.py
@@ -4,6 +4,54 @@ import sqlite3
 
 class ResetPassword(QtWidgets.QFrame):
     def __init__(self):
-        super().__init__
-        self.windowTitle('STD-Manager -> Reset Password')
-        print('yees')
+        super().__init__()
+        self.resize(800, 650)
+
+        #labels
+        self.log_title = QtWidgets.QLabel('CHANGE PASSWORD', self)
+        self.log_title.setGeometry(150, 70, 500, 90)
+        self.log_title.setObjectName('log_title')
+        #inputs
+        self.inp_npass = QtWidgets.QLineEdit(self)
+        self.inp_npass.setGeometry(300, 350, 200, 50)
+        self.inp_npass.setObjectName('input_logpg')
+        self.inp_npass.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)   
+        #buttons
+        self.btn_change = QtWidgets.QPushButton(self)
+        self.btn_change.setGeometry(300, 450, 200, 40)
+        self.btn_change.setText('CHANGE')
+        self.btn_change.setObjectName('btn_log')
+        self.btn_change.clicked.connect(self.reset_password)
+    
+    def reset_password(self):
+
+        #Check If Password is Strong
+        Number = False
+        Upper = False
+        Symbol = False
+        Strong = False
+
+        if len(self.inp_npass.text()) >= 8:
+            for l in self.inp_npass.text():
+                if l.isnumeric():
+                    Number = True
+                if l.isupper():
+                    Upper = True
+                if l in '?!@#%^&*+_-=':
+                    Symbol = True
+            if Number == True:
+                if Upper == True:
+                    if Symbol == True:
+                        Strong = True
+                    else:
+                        print('No symbol in your password')
+                else:
+                    print('No upper case in your password')
+            else:
+                print('No Number in your password:')
+        else:
+            print('<8 char.')
+
+    # Insert new password in your DataBase file
+
+    
