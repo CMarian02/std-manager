0b559ccdb9f2fbde1965986c57bad4c11e9a26b1
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2023-03-30 01:13:50 +0300

    a new way to login, now you have to log in with your CNP and Password

--- main.py
+++ main.py
@@ -43,9 +43,9 @@ class MyApp(QtWidgets.QMainWindow):
         self.info_text.setGeometry(600, 200, 190, 300)
         self.info_text.setObjectName('info_text')
         # intputs
-        self.inp_name = QtWidgets.QLineEdit(self.centralwidget)
-        self.inp_name.setGeometry(280, 260, 200, 40)
-        self.inp_name.setObjectName('input_logpg')
+        self.inp_cnp = QtWidgets.QLineEdit(self.centralwidget)
+        self.inp_cnp.setGeometry(280, 260, 200, 40)
+        self.inp_cnp.setObjectName('input_logpg')
         self.inp_pass = QtWidgets.QLineEdit(self.centralwidget)
         self.inp_pass.setGeometry(280, 320, 200, 40)
         self.inp_pass.setObjectName('input_logpg')
@@ -58,32 +58,35 @@ class MyApp(QtWidgets.QMainWindow):
         self.btn_log.setObjectName('btn_log')
         self.btn_log.clicked.connect(self.check_user)
 
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key.Key_Enter or event.key() == QtCore.Qt.Key.Key_Return:
+            self.check_user()
     def check_user(self):
         conn = sqlite3.connect('data/users.db')
         cursor = conn.cursor()
-        usernames = []
-        for user in cursor.execute('SELECT "Last Name" FROM all_users'):
-            usernames.append(user)
-        for username in usernames:
-            if str(username[0]).lower() == self.inp_name.text().lower():
-                for pas in cursor.execute('SELECT Password FROM all_users WHERE "Last Name" = (?)', (self.inp_name.text().capitalize(),)):
+        cnps = []
+        for user in cursor.execute('SELECT "CNP" FROM all_users'):
+            cnps.append(user)
+        for cnp in cnps:
+            if str(cnp[0])== self.inp_cnp.text():
+                for pas in cursor.execute('SELECT Password FROM all_users WHERE "CNP" = (?)', (self.inp_cnp.text(),)):
                     vfy_pass = str(pas[0])
                 if vfy_pass == self.inp_pass.text():
-                    for student in cursor.execute('SELECT Student FROM all_users WHERE "Last Name" = (?)', (self.inp_name.text().capitalize(),)):
+                    for student in cursor.execute('SELECT Student FROM all_users WHERE "CNP" = (?)', (self.inp_cnp.text(),)):
                         is_student = student[0]
                     if is_student == "Yes":
-                        for first in cursor.execute('SELECT First_time FROM all_users WHERE "Last Name" = (?)', (self.inp_name.text().capitalize(),)):
+                        for first in cursor.execute('SELECT First_time FROM all_users WHERE "CNP" = (?)', (self.inp_cnp.text(),)):
                             first_time = first[0]
                         if first_time == "Yes":
-                            reset_frame = ResetPassword(self.inp_name.text())
+                            reset_frame = ResetPassword(self.inp_cnp.text())
                             self.setCentralWidget(reset_frame)
                         else:
                             print('go to main app')
                     else:
-                        for first in cursor.execute('SELECT First_time FROM all_users WHERE "Last Name" = (?)', (self.inp_name.text().capitalize(),)):
+                        for first in cursor.execute('SELECT First_time FROM all_users WHERE "CNP" = (?)', (self.inp_cnp.text(),)):
                             first_time = first[0]
                         if first_time == "Yes":
-                            reset_frame = ResetPassword(self.inp_name.text())
+                            reset_frame = ResetPassword(self.inp_cnp.text())
                             self.setCentralWidget(reset_frame)
                         else:
                             print('go to main app')
@@ -95,7 +98,72 @@ class MyApp(QtWidgets.QMainWindow):
         conn.commit()
         cursor.close()
         conn.close()
-            
+
+class ResetPassword(QtWidgets.QFrame):
+    def __init__(self, cnp_input):
+        super().__init__()
+        self.resize(800, 650)
+        self.cnp_input = cnp_input
+        #labels
+        self.log_title = QtWidgets.QLabel('CHANGE PASSWORD', self)
+        self.log_title.setGeometry(150, 70, 500, 90)
+        self.log_title.setObjectName('log_title')
+        #inputs
+        self.inp_npass = QtWidgets.QLineEdit(self)
+        self.inp_npass.setGeometry(300, 350, 200, 50)
+        self.inp_npass.setObjectName('input_logpg')
+        self.inp_npass.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)   
+        #buttons
+        self.btn_change = QtWidgets.QPushButton(self)
+        self.btn_change.setGeometry(300, 450, 200, 40)
+        self.btn_change.setText('CHANGE')
+        self.btn_change.setObjectName('btn_log')
+        self.btn_change.clicked.connect(self.reset_password)
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key.Key_Enter or event.key() == QtCore.Qt.Key.Key_Return:
+            self.reset_password()
+    def reset_password(self):
+
+        #Check If Password is Strong
+        Number = False
+        Upper = False
+        Symbol = False
+        Strong = False
+
+        if len(self.inp_npass.text()) >= 8:
+            for l in self.inp_npass.text():
+                if l.isnumeric():
+                    Number = True
+                if l.isupper():
+                    Upper = True
+                if l in '?!@#%^&*+_-=':
+                    Symbol = True
+            if Number == True:
+                if Upper == True:
+                    if Symbol == True:
+                        Strong = True
+                    else:
+                        print('No symbol in your password')
+                else:
+                    print('No upper case in your password')
+            else:
+                print('No Number in your password:')
+        else:
+            print('<8 char.')
+
+        # Insert new password in your DataBase file
+
+        if Strong == True:
+            conn = sqlite3.connect('data/users.db')
+            cursor = conn.cursor()
+            cursor.execute('UPDATE all_users SET First_Time = "No" WHERE "CNP" = (?)', (self.cnp_input,))
+            cursor.execute('UPDATE all_users SET Password = (?) WHERE "CNP" = (?)', (self.inp_npass.text(), self.cnp_input))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            print('Your data is updated! Now you go to Main Page')
+        
 
 #Running App
 
