241cf92a10a694e0b3249b4e50ac63336134874e
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2023-04-03 18:26:27 +0300

    logo switch for your fac.

--- pages.py
+++ pages.py
@@ -3,7 +3,7 @@ from PyQt6 import QtWidgets, QtCore, QtGui
 import sqlite3
 
 class AppWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, cnp, group):
         super().__init__()
         self.setWindowTitle('STD-Manager')
         self.resize(1000,850)
@@ -22,64 +22,86 @@ class AppWindow(QtWidgets.QMainWindow):
         self.vers_text = QtWidgets.QLabel('version 0.1', self.centralwidget)
         self.vers_text.setGeometry(935, 680, 70, 20)
         self.vers_text.setObjectName('version_text')
-        #self.table_border = QtWidgets.QLabel(self.centralwidget)
-        #self.table_border.setGeometry(100, 100, 880, 560)
-        #self.table_border.setObjectName('table_border')
+        self.table_border = QtWidgets.QLabel(self.centralwidget)
+        self.table_border.setGeometry(100, 100, 880, 560)
+        self.table_border.setObjectName('table_border')
         self.fac_logo = QtWidgets.QLabel(self.centralwidget)
         self.fac_logo.setGeometry(0, 0, 80, 60)
-        self.fac_logo.setObjectName('fac_logo')
+        self.put_logo(group)
 
         #buttons
         self.year1 = QtWidgets.QPushButton(self.centralwidget)
         self.year1.setText('YEAR I')
         self.year1.setGeometry(130,0, 110, 60)
         self.year1.setObjectName('year_btn')
+        self.year1.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.year2 = QtWidgets.QPushButton(self.centralwidget)
         self.year2.setText('YEAR II')
         self.year2.setGeometry(250,0, 110, 60)
         self.year2.setObjectName('year_btn')
+        self.year2.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.year3 = QtWidgets.QPushButton(self.centralwidget)
         self.year3.setText('YEAR III')
         self.year3.setGeometry(370,0, 110, 60)
         self.year3.setObjectName('year_btn')
+        self.year3.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.year4 = QtWidgets.QPushButton(self.centralwidget)
         self.year4.setText('YEAR IV')
         self.year4.setGeometry(490,0, 110, 60)
         self.year4.setObjectName('year_btn')
+        self.year4.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.year5 = QtWidgets.QPushButton(self.centralwidget)
         self.year5.setText('YEAR V')
         self.year5.setGeometry(610,0, 110, 60)
         self.year5.setObjectName('year_btn')
+        self.year5.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.year6 = QtWidgets.QPushButton(self.centralwidget)
         self.year6.setText('YEAR VI')
         self.year6.setGeometry(730,0, 110, 60)
         self.year6.setObjectName('year_btn')
+        self.year6.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.year7 = QtWidgets.QPushButton(self.centralwidget)
         self.year7.setText('ALL YEARS')
         self.year7.setGeometry(850,0, 110, 60)
         self.year7.setObjectName('year_btn')
+        self.year7.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.btn1 = QtWidgets.QPushButton(self.centralwidget)
         self.btn1.setGeometry(0, 160, 80, 80)
         self.btn1.setObjectName('year_btn')
+        self.btn1.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.btn2 = QtWidgets.QPushButton(self.centralwidget)
         self.btn2.setGeometry(0, 280, 80, 80)
         self.btn2.setObjectName('year_btn')
+        self.btn2.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.btn3 = QtWidgets.QPushButton(self.centralwidget)
         self.btn3.setGeometry(0, 400, 80, 80)
         self.btn3.setObjectName('year_btn')
+        self.btn3.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.btn4 = QtWidgets.QPushButton(self.centralwidget)
         self.btn4.setGeometry(0, 520, 80, 80)
         self.btn4.setObjectName('year_btn')
+        self.btn4.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
 
-        #Table
-        self.table = QtWidgets.QTableWidget(5,4, self.centralwidget)
-        self.table.setFixedSize(880,560) 
-        self.table.setGeometry(100, 100, 880, 560)
-        self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeMode.Stretch)
-        self.table.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeMode.Stretch)
-        self.table.horizontalHeader().setVisible(False)
-        self.table.verticalHeader().setVisible(False)
-       
+
+
+    def put_logo(self,group):
+        
+        if group[0] == '1':
+            self.fac_logo.setObjectName('CH')
+        elif group[0] == '2':
+            self.fac_logo.setObjectName('MEC')
+        elif group[0] == '3':
+            self.fac_logo.setObjectName('CI')
+        elif group[0] == '4':
+            self.fac_logo.setObjectName('DIMA')
+        elif group[0] == '5':
+            self.fac_logo.setObjectName('ETTI')
+        elif group[0] == '6':
+            self.fac_logo.setObjectName('IEEIA')
+        elif group[0] == '7':
+            self.fac_logo.setObjectName('AC')
+        else:
+            self.fac_logo.setObjectName('app_logo')
 
 
         
